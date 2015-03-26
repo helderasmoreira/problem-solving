d3ddf8daea9408c3acf3c68ac95e6b3fba467a6d
Hélder Moreira <heldersantos.moreira@gmail.com>
2015-03-26 15:17:26 +0000

    Add Cookie Clicker Alpha.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Sometimes I might not have come up with the most efficient way of doing things, 
  * [A. Alien Language][#2009-Q-A] (Qualification)
 
 #### Code Jam 2014
+* [B. Cookie Clicker Alpha][#2014-Q-B] (Qualification)
 * [D. Deceitful War][#2014-Q-D] (Qualification)
 * [1A-A. Charging Chaos][#2014-1A-A] (1A - A)
 
@@ -20,6 +21,7 @@ Write a problem to find the right combination of numbers that makes the sentence
 ```
 
 [#2009-Q-A]: http://code.google.com/codejam/contest/dashboard?c=90101#s=p0
+[#2014-Q-B]:https://code.google.com/codejam/contest/2974486/dashboard#s=p1
 [#2014-Q-D]:https://code.google.com/codejam/contest/2974486/dashboard#s=p3
 [#2014-1A-A]: https://code.google.com/codejam/contest/2984486/dashboard#s=p0&a=0
 [#callforwarding]:http:www.reddit.com/r/dailyprogrammer/comments/1g09qy/060913_challenge_127_intermediate_call_forwarding/
---
+++ ruby/cookie_clicker_alpha/cookie_cliker_alpha.rb
@@ -0,0 +1,51 @@
+require 'benchmark'
+
+class CookieClickerAlpha
+  def solve(dataset)
+    File.open(dataset, 'r') do |input|
+      File.open(dataset.sub(/\.in/, '.out'), 'w') do |output|
+        test_cases = input.readline.to_i
+        1.upto(test_cases) do |test_case|
+          line = input.readline.split.map { |n| n.to_f }
+          result = analyze(line[0], line[1], line[2])
+          output << %Q{Case ##{test_case}: #{result}\n}
+        end
+      end
+    end
+  end
+
+  def analyze_aux(current_time, cookies_per_second, cost, farm, goal)
+    time_to_next_farm = cost / cookies_per_second
+    time_to_goal = goal / cookies_per_second
+    time_to_goal_after_next_farm = goal / (cookies_per_second + farm)
+    if time_to_goal > (time_to_next_farm + time_to_goal_after_next_farm)
+      analyze_aux(
+        current_time + time_to_next_farm,
+        cookies_per_second + farm,
+        cost,
+        farm,
+        goal
+      )
+    else
+      (current_time + time_to_goal).round(7)
+    end
+  end
+
+  def analyze(cost, farm, x)
+    cookies_per_second = 2.0
+    analyze_aux(
+      0.0,
+      cookies_per_second,
+      cost,
+      farm,
+      x
+    )
+  end
+end
+
+# still some problems in the large dataset
+problem = CookieClickerAlpha.new
+Benchmark.bm do |x|
+  x.report('small') { problem.solve('B-small-practice.in') }
+  #x.report('large') { problem.solve('B-large-practice.in') }
+end
