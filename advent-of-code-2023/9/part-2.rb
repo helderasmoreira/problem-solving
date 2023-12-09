1eb6825eddfe23d5f91ab5174d0dce71cc58c0f4
Hélder Moreira <heldersantos.moreira@gmail.com>
2023-12-09 13:06:23 +0000

    AoC 2023: day 9

---
+++ advent-of-code-2023/9/part-2.rb
@@ -0,0 +1,15 @@
+require 'pry-byebug'
+
+def difference(numbers)
+  return 0 if numbers.uniq.size == 1
+
+  new_numbers = numbers.each_cons(2).map { |one, other| other - one }
+  new_numbers.first - difference(new_numbers)
+end
+
+values = File.readlines('puzzle.input', chomp: true).map do |line|
+  numbers = line.split(' ').map(&:to_i)
+  numbers.first - difference(numbers)
+end
+
+puts values.reduce(:+)
