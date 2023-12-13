88d2d5df5c515d0c45e695bb0eec207f3d803850
Hélder Moreira <heldersantos.moreira@gmail.com>
2023-12-13 00:35:31 +0000

    Add comments to day 12 for posterity

--- advent-of-code-2023/12/part-2.rb
+++ advent-of-code-2023/12/part-2.rb
@@ -13,23 +13,37 @@ def analyse(l, r, nr_w)
 
   case l[0]
   when nil
+    # if there's no more string
+    # if right side is empty or if right side is a match to the last set of #, it's a good one
+    # if not, it's a bad one and does not match
     (r.empty? || (r.size == 1 && nr_w == r[0])) ? 1 : 0
   when '.'
     if nr_w.zero?
+      # if we were not in the middle of a # streak, just move on
       analyse_and_cache(l_tail, r, 0)
     elsif nr_w == r[0]
+      # if we were in the middle of a # streak, consume that number from the right because it matches
+      # it is still a possible case
       analyse_and_cache(l_tail, r[1..], 0)
     else
+      # if we were in the middle of a # streak that ended and does not match
+      # it's a bad scenario
       0
     end
   when '#'
+    # if there is no right side at this point, it's a bad scenario
     return 0 if r.nil? || r.empty?
 
     nr_w += 1
+    # we've seen one more #, the number needs to still be lower or equal to the next right block
+    # for it to continue in contention
     return 0 if nr_w > r[0]
 
+    # consume one from the left, keep analysing
     analyse_and_cache(l_tail, r, nr_w)
   when '?'
+    # it could be a # or a .
+    # so let's fork and add the possibilities for the two scenarios
     analyse_and_cache(['#'] + l_tail, r, nr_w) + analyse_and_cache(['.'] + l_tail, r, nr_w)
   end
 end
