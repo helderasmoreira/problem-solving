5513196841a4274ae1b39c558a86528a7c18e38d
Hélder Moreira <heldersantos.moreira@gmail.com>
2024-12-08 10:58:00 +0000

    AoC 2024: day 7

---
+++ advent-of-code-2024/7/part-2.rb
@@ -0,0 +1,30 @@
+require 'pry-byebug'
+
+possible = []
+
+File.readlines('puzzle.input', chomp: true).each do |line|
+  total, rest = line.split(': ')
+  rest = rest.split(' ').map(&:to_i)
+  total = total.to_i
+
+  i = 1
+  options = [rest.first]
+  loop do
+    break if i == rest.size
+
+    new_options = []
+
+    options.each do |option|
+      new_options << option + rest[i]
+      new_options << option * rest[i]
+      new_options << "#{option}#{rest[i]}".to_i
+    end
+
+    options = new_options
+    i += 1
+  end
+
+  possible << total if options.include? total
+end
+
+puts possible.sum
